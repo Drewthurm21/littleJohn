b5e1b7f52cfed78e4f0c9109cf7b3bfd625139c2
Drewthurm21 <drewthurm21@gmail.com>
2023-04-18 18:35:11 -0500

    creating repo w/ project skeleton, user and portfolio db models - will continue on with db

---
+++ app/models/portfolio.py
@@ -0,0 +1,17 @@
+from .db import db
+
+
+class Portfolio(db.Model):
+    __tablename__ = 'portfolios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    balance = db.Column(db.Numeric(asdecimal=False), nullable=False)
+    
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'owner_id': self.owner_id,
+            'balance': self.balance
+        }
