ab3eb5c6ec11c93694298681694561d0668945bc
Drewthurm21 <drewthurm21@gmail.com>
2023-04-18 19:26:48 -0500

    added db schema to assets. created db model for watch lists

---
+++ app/models/watchlist.py
@@ -0,0 +1,16 @@
+from .db import db
+
+
+class Watchlist(db.model):
+    __tablename__ = 'portfolios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.foreignKey('users.id'), nullable=False)
+    name = db.Column(db.String(90), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'owner_id': self.owner_id,
+            'name': self.name
+        }
