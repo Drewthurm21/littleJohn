9b2fab9766e6cab60929d5fb9bd356f183f73743
Drewthurm21 <drewthurm21@gmail.com>
2023-04-18 19:54:01 -0500

    added db model for watchlist item

---
+++ app/models/watchlist_item.py
@@ -0,0 +1,17 @@
+from .db import db
+
+
+class Watchlist_Item(db.Model):
+    __tablename__ = 'watchlist_item'
+
+    id = db.Column(db.Integer, primary_key=True)
+    watchlist_id = db.Column(db.Integer, db.foreignKey(
+        'watchlist.id'), nullable=False)
+    ticker = db.Column(db.String(90), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'watchlist_id': self.watchlist_id,
+            'ticker': self.ticker
+        }
