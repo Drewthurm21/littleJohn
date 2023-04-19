3b4c4b07ff0a693545d48b61b95f0ab26687d5f4
Drewthurm21 <drewthurm21@gmail.com>
2023-04-19 13:30:13 -0500

    added an example env.py to migrations - refactored remaining files for render deployment

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,4 +1,5 @@
 from flask.cli import AppGroup
+from app.models import db, environment, SCHEMA
 from .users import seed_users, undo_users
 from .watchlists import seed_watchlists, undo_watchlists
 from .portfolios import seed_portfolios, undo_portfolios
@@ -13,6 +14,16 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
+        # the schema name. This is because the schema name is
+        # prepended to the table name in production.
+        undo_trades()
+        undo_portfolios()
+        undo_watchlist_items()
+        undo_watchlists()
+        undo_users()
     seed_users()
     seed_watchlists()
     seed_watchlist_items()
